9dd4a0d52ab4d47b50e132325bf9dd2215db1a9c
elena-lucky <elena.ivanovada@gmail.com>
2023-08-14 18:00:59 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    let num = Math.abs(n).toString();
+    let result = '';
+    let i = 0;
+    while (i < num.length) {
+      result = `${num[i]}${result}`;
+      i = i + 1;
+    }
+    return result;
+  };
   
-}
+ 
